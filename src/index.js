5946d1b2e4d4bc07c38a97c02107345747dcb7f7
Temitope <tope_olajide@outlook.com>
2018-10-30 13:39:48 +0100

    create store in index.js file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { createStore } from 'redux';
 
-ReactDOM.render(
-<App />,document.getElementById('root'));
+const store = createStore();
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
