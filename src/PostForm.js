bf29b5690d7842bb379495a6e27a8c3d54369d4a
Temitope <tope_olajide@outlook.com>
2018-10-30 13:54:50 +0100

    Add handleSubmit method

--- src/PostForm.js
+++ src/PostForm.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 
 class PostForm extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const title = this.getTitle.value;
+    const message =  this.getMessage.value;
+    const data = {
+      id: new Date(),
+      title,
+      message
+    }
+  }
 render() {
 return (
 <div>
   <h1>Create Post</h1>
-  <form>
-   <input required type="text" placeholder="Enter Post Title" /><br /><br />
-   <textarea required rows="5" cols="28" placeholder="Enter Post" /><br /><br />
+  <form onSubmit={this.handleSubmit}>
+   <input required type="text" ref={(input)=>this.getTitle = input} 
+    placeholder="Enter Post Title"/>
+   <br /><br />
+   <textarea required rows="5" ref={(input)=>this.getMessage = input} cols="28" 
+    placeholder="Enter Post" />
+   <br /><br />
    <button>Post</button>
   </form>
 </div>
