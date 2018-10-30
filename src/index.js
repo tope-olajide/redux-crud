aff7ba894ea0fb09a06cc300ff0b45d59176c1ac
Temitope <tope_olajide@outlook.com>
2018-10-30 13:53:02 +0100

    Connect store to all the component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-const store = createStore();
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import postReducer from './reducers/postReducer';
+const store = createStore(postReducer);
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>, document.getElementById('root'));
